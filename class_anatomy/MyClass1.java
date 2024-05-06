394973abca51f2402c5f0caeee1275136d98316c
Gabriel <glimone.nunes@outlook.com>
2024-05-06 20:34:08 -0300

    Estrutura básica de uma class

---
+++ class_anatomy/MyClass1.java
@@ -0,0 +1,12 @@
+//É padrão que a classe seja formada por:
+//public class nome_classe  { CÓDIGO }
+
+public class MyClass1 {
+    
+    public static void main (String [] args){
+
+        
+
+    }
+     
+}
