11d9c21329102a891207dbe119194311806ac3d7
Gabriel <glimone.nunes@outlook.com>
2024-05-06 21:29:25 -0300

    Definindo uma VAR inalterável

--- class_anatomy/src/MyClass2.java
+++ class_anatomy/src/MyClass2.java
@@ -1,3 +1,10 @@
+//Conceito de variável inalterável 
+
 public class MyClass2 {
+
+final string BR = "Brasil";
+//Por padrão, quando definimos uma variável com "Final", estamos dizendo que essa variável é inalterável. Normalmente detectamos que é inalterável pois a variável foi dada em letra maíuscula, no exemplo "BR".
+ 
+BR = "Brasil";
     
 }
