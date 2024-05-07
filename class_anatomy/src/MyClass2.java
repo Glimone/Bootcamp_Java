fc39e93e02addada461ca4fa4edb3778aca679c7
Gabriel <glimone.nunes@outlook.com>
2024-05-06 21:31:43 -0300

    Correção do código

--- class_anatomy/src/MyClass2.java
+++ class_anatomy/src/MyClass2.java
@@ -1,10 +1,9 @@
-//Conceito de variável inalterável 
+ public class MyClass2 {
 
-public class MyClass2 {
-
+    public static void main (String [] args) {
 final string BR = "Brasil";
-//Por padrão, quando definimos uma variável com "Final", estamos dizendo que essa variável é inalterável. Normalmente detectamos que é inalterável pois a variável foi dada em letra maíuscula, no exemplo "BR".
- 
+
 BR = "Brasil";
-    
+}
+
 }
