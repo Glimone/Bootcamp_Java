6c36f739bd618618b2c48bde891577c85c626774
Gabriel <glimone.nunes@outlook.com>
2024-05-07 18:52:19 -0300

    Estrutura de uma variável com exemplo

--- class_anatomy/src/MyClass2.java
+++ class_anatomy/src/MyClass2.java
@@ -1,9 +1,8 @@
  public class MyClass2 {
 
     public static void main (String [] args) {
-final string BR = "Brasil";
-
-BR = "Brasil";
+        final string BR = "Brasil";
+        BR = "Brasil";
 }
 
 }
---
+++ class_anatomy/src/MyClass3.java
@@ -0,0 +1,11 @@
+public class MyClass3 {
+     public static void main ( String [] args) {
+        /*A estrutura padrão de uma var em java é:
+tipo VarName = Condition  
+Ex:
+*/      String testetext = "Olá, eu sou uma var de string!";
+        System.out.print(testetext);
+
+     } 
+}
+
