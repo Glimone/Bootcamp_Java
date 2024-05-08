1f85c312e9afcead606d1081d2976ede917622c8
Gabriel <glimone.nunes@outlook.com>
2024-05-08 17:38:22 -0300

    Criação de novo método

--- class_anatomy/src/MyClass3.java
+++ class_anatomy/src/MyClass3.java
@@ -1,16 +1,13 @@
 public class MyClass3 {
+        
+     //Isso é um método, o método principal   
      public static void main ( String [] args) {
-        //Alguns tipos de variáveis são:
-        String text = "Esse texto é uma var string, e esse numero é de uma VAR int: ";
-        int number = 25; //TIpo inteiro
         
-        System.out.println(text + number);
-
-        Boolean YN = true;  //Tipo Boolean
-        System.out.println(YN);
-        
+}    
+        //Isso é outro método, agora nossa class tem 2 métodos.
+     public static String name (String firstName, String lastName) {
 
 
-     } 
-}
+} 
+
 
