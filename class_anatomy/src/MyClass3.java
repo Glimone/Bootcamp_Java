03aca8c6e51719163dd63ca5bcd2cfa23247e4f6
Gabriel <glimone.nunes@outlook.com>
2024-05-08 17:16:38 -0300

    Alguns tipos de var

--- class_anatomy/src/MyClass3.java
+++ class_anatomy/src/MyClass3.java
@@ -1,11 +1,16 @@
 public class MyClass3 {
      public static void main ( String [] args) {
-        /*A estrutura padrão de uma var em java é:
-tipo VarName = Condition  
-Ex:
-*/      String testetext = "Olá, eu sou uma var de string!";
-        System.out.print(testetext);
--
+        //Alguns tipos de variáveis são:
+        String text = "Esse texto é uma var string, e esse numero é de uma VAR int: ";
+        int number = 25; //TIpo inteiro
+        
+        System.out.println(text + number);
+
+        Boolean YN = true;  //Tipo Boolean
+        System.out.println(YN);
+        
+
+
      } 
 }
 
