20ca053622c6042b0999242a0009fc39f19075e4
Gabriel <glimone.nunes@outlook.com>
2024-05-08 18:06:43 -0300

    Criação de método com parâmetros e return

--- class_anatomy/src/MyClass3.java
+++ class_anatomy/src/MyClass3.java
@@ -2,12 +2,24 @@ public class MyClass3 {
         
      //Isso é um método, o método principal   
      public static void main ( String [] args) {
-        
+          String first = "Gabriel";; //Definição de variáveis que funcionarão como parâmetros
+          String last = "Limone Nunes";
+
+           //var que define ser igual ao método name
+          String completeName = name (first, last); //Método name com os parâmetros definidos.
+         
+          System.out.println(completeName); //Impressão da variável que contém o método e parâmetros. 
+
+     
 }    
-        //Isso é outro método, agora nossa class tem 2 métodos.
+       
+//Isso é outro método, agora nossa class tem 2 métodos.
      public static String name (String firstName, String lastName) {
 
+          return firstName + " " + lastName; //Comando de retorno para o método main
 
 } 
 
+
+}
 
