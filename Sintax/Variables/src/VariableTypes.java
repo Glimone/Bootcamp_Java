9d6033d7396aa9ec617b7487ec6f853bc1a0472a
Gabriel <glimone.nunes@outlook.com>
2024-05-09 20:59:37 -0300

    Tipos de variaveis com comentários

--- Sintax/Variables/src/VariableTypes.java
+++ Sintax/Variables/src/VariableTypes.java
@@ -1,12 +1,33 @@
 public class VariableTypes {
     
     public static void main(String[] args) {
-        /*Como padrão, seguimos esse padrão de variável:
+        /*How pattern, we follow this variable structure:
           
         typeVar nameVar = value;
         */
 
+        //Variable Types: 
+
+
+        String texto = "That's a String"; //Type String
+        System.out.println(texto);
         
+        int number = 2600; //Type integral
+        System.out.println(number);
+         
+        double decimal = 32.455; //Type big decimals
+        System.out.println(decimal);
+
+        float smallDecimal = 3.14F; //Type small decimals, necessary the "F" ever.
+        System.out.println(smallDecimal);
+        
+        long hugenumber = 3423434354353L; //Type long number, necessary the "L" ever.
+        System.out.println(hugenumber);
+
+        final String FINISH = " Essa string foi definida para nunca ser mudado, com 'final'."; //In main, we write the var final with capsLock on.
+        System.out.println(FINISH);
+
+
 
     }
 
