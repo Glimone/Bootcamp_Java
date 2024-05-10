5dd60d30dd93122ad3f558815f951ec16f001b46
Gabriel <glimone.nunes@outlook.com>
2024-05-10 08:54:19 -0300

    Basic operators and .gitignore

---
+++ Sintax/Operators/src/BasicOperator.java
@@ -0,0 +1,11 @@
+public class BasicOperator {
+    public static void main(String[] args) throws Exception {
+        //The aritmetic operators its the same of others languages, how:
+        
+        // +(sum), -(adition), *(multiplication), %(module), /(division, ==(equal), 
+
+        //The + and - make de concatenation of Strings.
+    
+
+    }
+}
