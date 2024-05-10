8f7008a5631a436f98be763f8d840961548fdb13
Gabriel <glimone.nunes@outlook.com>
2024-05-10 18:03:15 -0300

    Ternary types with exemple

---
+++ Sintax/Operators/src/Ternary.java
@@ -0,0 +1,30 @@
+public class Ternary {
+    
+    public static void main(String[] args) {
+        
+        //New, we see how is make a ternary structure.
+
+        //First, we want know the simbols:
+
+        // The '?' its equal the 'if', of conditional structure.
+
+        //The ':' its equal the 'if else'. Exemple:
+
+        int num1 = 34;
+        int num2 = 54;
+        int num3 = 13;
+        int num4 = 45;
+
+        int numx = num1 * num3;
+        int numy = num2 * num4;
+
+        String result = numx < numy ? "The bigger result is num1 and num3" : "The bigger result is num2 and num 3";
+
+        System.out.println(result);
+
+
+
+    }
+
+    
+}
