97fd0fc037a047d78719c7b39d62c10e9cde18ad
Gabriel <glimone.nunes@outlook.com>
2024-05-10 16:34:23 -0300

    Feature

--- Sintax/class_anatomy/src/HelloWorld.java
+++ Sintax/class_anatomy/src/HelloWorld.java
@@ -2,16 +2,7 @@ public class HelloWorld {
     
     public static void main (String [] args){
         System.out.print("Meu prmeiro código JAVA!");
-<<<<<<< HEAD
       
-=======
-        //System é uma classe de sistema, com diversas operações
-
-        //.out significa "Saída"
-        //print é a saída de impressão.
-
-        //Isso é o mesmo que dizer: "Sistema, me de uma saída que será uma impressão"
->>>>>>> new-feature
 }
 
 } 
