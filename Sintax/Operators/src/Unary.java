5f4c9bf939126ba4884cb1f636804866455bdb12
Gabriel <glimone.nunes@outlook.com>
2024-05-10 16:49:29 -0300

    Comments of unary operators.

---
+++ Sintax/Operators/src/Unary.java
@@ -0,0 +1,15 @@
+public class Unary {
+    
+    public static void main (Strings [] args) {
+        /*The unary values:
+        + - positive
+        - - Negative
+        ++ - add +1
+        -- - subtract -1
+        ! - Recuse a value boolean.
+        */
+
+
+    }
+
+}
