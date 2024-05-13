bf3e3b75617983cae0fc4487f653fec75d51ec9a
Gabriel <glimone.nunes@outlook.com>
2024-05-13 15:43:20 -0300

    Explanation AND OR

---
+++ Sintax/Operators/src/Logicals.java
@@ -0,0 +1,13 @@
+public class Logicals {
+
+
+    public static void main (String [] args) [
+    //The logical operators are: AND or OR. The AND need everyone to be true to return true. The OR need only 1 conditions to return true.
+
+    //Symbols: AND = || OR = &&
+
+    
+
+    ]
+    
+}
