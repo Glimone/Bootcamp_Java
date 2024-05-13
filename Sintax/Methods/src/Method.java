bc872cf82475940a4ee5167d7dd13091c829b1c3
Gabriel <glimone.nunes@outlook.com>
2024-05-13 18:09:49 -0300

    Estrutura e principais regras de métodos.

---
+++ Sintax/Methods/src/Method.java
@@ -0,0 +1,17 @@
+public class Method {
+    public static void main(String[] args) throws Exception {
+        /*A estrutura de um método é a seguinte:
+         
+        type methodName = (adicione parâmetros se precisar) {...}
+        
+        Os métodos possuem alguns tipos de visibilidade em JAVA,eles são: Geral, herança, pacotes definidos ou classes.
+
+        Existem situações com exceção dentro de um método, que não devem existir, então atenção nisso.
+
+        Quando a classe não tiver retorno, deve-se usar a palavra-chave 'void'.
+         
+         */
+
+        //Quando for se criar um método invisível para outras classes, deve-se usar o método em 'private'.
+    }
+}
