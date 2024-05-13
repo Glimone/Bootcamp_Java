89fbf0e77f26e60ac4f7fcdf48f9490b80794f32
Gabriel <glimone.nunes@outlook.com>
2024-05-13 08:49:29 -0300

    Relationals with exemples

--- Sintax/Operators/src/Relationals.java
+++ Sintax/Operators/src/Relationals.java
@@ -1,18 +1,34 @@
 public class Relationals {
     
-    public static void main (String [] args) {
-        /* The relational types are symbols which the objective is the relationship and comparison of two or more sentences. They are:
-          
-        > - Bigger then
+    public static void main (String [] args) {  
+        /* > - Bigger then
         >= - Bigger or equal then
         < - Less then 
         <= - Less or equal then
         == - Equal
         != Different
-         
+        */
+
+        //Exemple 1:
+
+        int int1 = 1;
+        int int2 = 2;
+
+        boolean simNao = int1 == int2;
+
+        System.out.println("The first number is equal the second number = " + simNao); //If you use the "!=", the result is true
+
+        //Exemple2:
+
+        float n1 = 34*3;
+        float n2 = 18*4;
+
+        System.out.println(n1 < n2); //With the ">", the result is true.
+
+        
 
 
-         */
+        
 
     }
 
