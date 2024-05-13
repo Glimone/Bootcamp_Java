8d606e48878e825e0233044c4fa5684e2fd48724
Gabriel <glimone.nunes@outlook.com>
2024-05-13 08:34:55 -0300

    The explanation of operators relatinals

---
+++ Sintax/Operators/src/Relationals.java
@@ -0,0 +1,20 @@
+public class Relationals {
+    
+    public static void main (String [] args) {
+        /* The relational types are symbols which the objective is the relationship and comparison of two or more sentences. They are:
+          
+        > - Bigger then
+        >= - Bigger or equal then
+        < - Less then 
+        <= - Less or equal then
+        == - Equal
+        != Different
+         
+
+
+         */
+
+    }
+
+
+}
