0042bc9e15ac506b9583afb64048bc2918fc8db1
Gabriel <glimone.nunes@outlook.com>
2024-05-17 21:54:51 -0300

    Scanner class

---
+++ Sintax/Arguments/src/AboutMe.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;  //Importação de classe Scanner
+
+public class AboutMe {
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next(); //Definição de var com retorno em scanner.
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble(); //Quando o var não for string, necessita informar junto com "next" o tipo de var.
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        
+        
+    }
+}
