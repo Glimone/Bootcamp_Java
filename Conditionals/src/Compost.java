55c472052752a26d7762e0a998ddd933665831fa
Gabriel <glimone.nunes@outlook.com>
2024-05-23 15:11:54 -0300

    Estruturas compostas e encadeads (If, else if e else)

---
+++ Conditionals/src/Compost.java
@@ -0,0 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Compost {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        double saldo = 1000.00;
+        System.out.println("Olá, o limite do seu empréstimo no momento é de " + saldo);
+
+        System.out.println("Digite quanto quer sacar: ");
+        double saque = scanner.nextDouble();
+
+        if(saque <= saldo) {
+            saldo = saldo - saque;
+            System.out.println("Você acaba de solicitar o saque de " + saque + " e seu limite disponível agora é de " + saldo + " compareça na sua agência contendo RG, número da conta e o valor do saque!");
+        }
+        else if(saque > saldo) {  //Estrutura encadeada
+            System.out.println("O valor de saque é maior do que seu limite, volte e digite novamente. ");
+
+
+        }
+        else { //No geral, isso é uma estrutura composta
+            System.out.println("Caractere inviável, tente novamente.");
+        }
+
+    
+    scanner.close();
+        
+
+    }
+
+}
--- Conditionals/src/Simple.java
+++ Conditionals/src/Simple.java
@@ -18,7 +18,7 @@ public class Simple {
         
         }
 
-        
+        scanner.close();
 
     }
     
