d1eb1f2fc32e0c60a50677ee08d1582045dd9251
Gabriel <glimone.nunes@outlook.com>
2024-07-06 12:36:57 -0300

    Feature

---
+++ POO/Fundamentos POO/Encapsulamento/src/pedroComputer.java
@@ -0,0 +1,12 @@
+package
+
+public class pedroComputer {
+    public static void main(String[] args) {
+		//abrindo MSN Messenger
+		msnMessenger msn = new msnMessenger();
+		
+		msn.enviarMensagem();
+		
+		msn.receberMensagem();
+	}
+}
